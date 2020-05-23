ae5af881a280a93f7b646504977dd84e9e5c90c8
Li Li <lili@mmlab.cs.tsukuba.ac.jp>
2020-05-23 14:59:47 +0900

    modify download

--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@ if [ $FILE == "dataset-VCC" ]; then
     URL=https://www.dropbox.com/s/eld8lz80uhy1zur/vcc.zip?dl=0
     ZIP_FILE=./data/vcc.zip
     mkdir -p ./data/
-    wget -N $URL -O $ZIP_FILE
+    wget -nv --show-progress $URL -O $ZIP_FILE
     unzip -qq $ZIP_FILE -d ./data/
     rm $ZIP_FILE
 
@@ -14,7 +14,7 @@ elif [ $FILE == "test-samples" ]; then
     URL=https://www.dropbox.com/s/0kk7swhs9o9weft/test_input.zip?dl=0
     ZIP_FILE=./data/test_input.zip
     mkdir -p ./data/
-    wget -N $URL -O $ZIP_FILE
+    wget -nv --show-progress $URL -O $ZIP_FILE
     unzip -qq $ZIP_FILE -d ./data/
     rm $ZIP_FILE
 
@@ -23,7 +23,7 @@ elif [ $FILE == "model-VCC" ]; then
     URL=https://www.dropbox.com/s/3phzus1moquxmly/model-vcc.zip?dl=0
     ZIP_FILE=./pretrained_model/vcc.zip
     mkdir -p ./pretrained_model/
-    wget -N $URL -O $ZIP_FILE
+    wget -nv --show-progress $URL -O $ZIP_FILE
     unzip -qq $ZIP_FILE -d ./pretrained_model/
     rm $ZIP_FILE
 
