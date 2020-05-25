a29b47ff17e0618e278bba2dd5d0fee0daeb99c5
Li Li <lili@mmlab.cs.tsukuba.ac.jp>
2020-05-25 14:38:05 +0900

    modify download

--- download.sh
+++ download.sh
@@ -1,34 +1,34 @@
 FILE=$1
-
+​
 if [ $FILE == "dataset-VCC" ]; then
     # VCC dataset including 4 speakers
-    URL=https://www.dropbox.com/s/eld8lz80uhy1zur/vcc.zip?dl=0
+    URL=http://www.kecl.ntt.co.jp/people/kameoka.hirokazu/data/mvae/vcc.zip
     ZIP_FILE=./data/vcc.zip
     mkdir -p ./data/
-    wget -nv --show-progress $URL -O $ZIP_FILE
+    wget --progress=bar -nv $URL -O $ZIP_FILE
     unzip -qq $ZIP_FILE -d ./data/
     rm $ZIP_FILE
-
+​
 elif [ $FILE == "test-samples" ]; then
     # test samples for VCC dataset
-    URL=https://www.dropbox.com/s/0kk7swhs9o9weft/test_input.zip?dl=0
+    URL=http://www.kecl.ntt.co.jp/people/kameoka.hirokazu/data/mvae/test_input.zip
     ZIP_FILE=./data/test_input.zip
     mkdir -p ./data/
-    wget -nv --show-progress $URL -O $ZIP_FILE
+    wget --progress=bar -nv $URL -O $ZIP_FILE
     unzip -qq $ZIP_FILE -d ./data/
     rm $ZIP_FILE
-
+​
 elif [ $FILE == "model-VCC" ]; then
     # pretrained model using VCC dataset
-    URL=https://www.dropbox.com/s/3phzus1moquxmly/model-vcc.zip?dl=0
+    URL=http://www.kecl.ntt.co.jp/people/kameoka.hirokazu/data/mvae/model-vcc.zip
     ZIP_FILE=./pretrained_model/vcc.zip
     mkdir -p ./pretrained_model/
-    wget -nv --show-progress $URL -O $ZIP_FILE
+    wget --progress=bar -nv $URL -O $ZIP_FILE
     unzip -qq $ZIP_FILE -d ./pretrained_model/
     rm $ZIP_FILE
-
+​
 else
     echo "Available arguments are dataset-VCC, test-samples, model-VCC."
     exit 1
-
+​
 fi
